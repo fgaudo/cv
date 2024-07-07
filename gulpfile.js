97c01e139c4655dc98ae2cac50a4d4d8283d0ca0
Francesco Gaudenzi <fgaudo@gmail.com>
2024-07-07 02:31:29 +0200

    dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 import path from "path";
 import Handlebars from "handlebars";
-import experiences from "./data/experiences.js";
 import { parallel, series, watch } from "gulp";
-import { mkdir, readFile, writeFile } from "fs/promises";
+import { mkdir, readFile, readdir, writeFile } from "fs/promises";
 import postcss from "postcss";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
@@ -14,20 +13,31 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 async function buildHtml() {
-  const template = await readFile(
-    path.join(__dirname, "templates/index.hbs"),
-    "utf-8",
+  const [template, nextrekTemplateNames] = await Promise.all([
+    readFile(path.join(__dirname, "templates/index.hbs"), "utf-8"),
+    readdir(path.join(__dirname, "data/nextrek")),
+  ]);
+
+  const nextrekTemplates = await Promise.all(
+    nextrekTemplateNames.map((a) =>
+      readFile(path.join(__dirname, "data/nextrek/", a), "utf-8"),
+    ),
   );
 
-  var renderTemplate = Handlebars.compile(template);
-  var html = renderTemplate(experiences);
+  const renderNextrekTemplates = nextrekTemplates
+    .map((a) => Handlebars.compile(a))
+    .map((render) => render());
 
-  await writeFile(path.join(__dirname, "dist/index.html"), html);
+  const renderTemplate = Handlebars.compile(template);
+
+  const html = renderTemplate({ nextrek: renderNextrekTemplates });
+
+  await writeFile(path.join(__dirname, "dist/index.xhtml"), html);
 }
 
 async function buildCss() {
   const cssFilePath = path.join(__dirname, "css/style.css");
-  const css = await readFile(cssFilePath);
+  const css = await readFile(cssFilePath, "utf-8");
   const processor = postcss(
     autoprefixer(),
     tailwindcss({
@@ -41,8 +51,7 @@ async function buildCss() {
       plugins: [],
     }),
   );
-  const cssOutputDir = path.join(__dirname, "dist/css");
-  const cssOutputFilePath = path.join(cssOutputDir, "style.css");
+  const cssOutputFilePath = path.join(__dirname, "dist/css/style.css");
 
   const result = await processor.process(css, {
     from: cssFilePath,
