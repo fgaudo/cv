cb7eae3975e18f8440cdc4a2448835d4aa366347
Francesco Gaudenzi <fgaudo@gmail.com>
2024-07-07 04:12:42 +0200

    dev

---
+++ data/info.js
@@ -0,0 +1,5 @@
+export default {
+  email: "fgaudo@gmail.com",
+  phone: "+41791750215",
+  address: "Beckenried (Switzerland)",
+};
--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ import path from "path";
 import Handlebars from "handlebars";
 import { parallel, series, watch } from "gulp";
 import { mkdir, readFile, readdir, writeFile } from "fs/promises";
+import info from "./data/info.js";
 import postcss from "postcss";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
-import { fileURLToPath } from "url";
+import { fileURLToPath, pathToFileURL } from "url";
 import { dirname } from "path";
 import { deleteAsync } from "del";
+import wkhtmltopdf from "wkhtmltopdf";
+import { createReadStream, createWriteStream } from "fs";
+import puppeteer from "puppeteer";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -30,7 +34,11 @@ async function buildHtml() {
 
   const renderTemplate = Handlebars.compile(template);
 
-  const html = renderTemplate({ nextrek: renderNextrekTemplates });
+  const html = renderTemplate({
+    info,
+    css: "./css/style.css",
+    nextrek: renderNextrekTemplates,
+  });
 
   await writeFile(path.join(__dirname, "dist/index.xhtml"), html);
 }
@@ -43,6 +51,7 @@ async function buildCss() {
     tailwindcss({
       content: [
         path.join(__dirname, "templates/*"),
+        path.join(__dirname, "data/*"),
         path.join(__dirname, "css/*"),
       ],
       theme: {
@@ -61,20 +70,35 @@ async function buildCss() {
   await writeFile(cssOutputFilePath, result.css);
 }
 
+export async function buildPDF() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const website_url = pathToFileURL(path.join(__dirname, "dist/index.xhtml"));
+  await page.goto(website_url, { waitUntil: "networkidle0" });
+  await page.emulateMediaType("screen");
+  await page.pdf({
+    path: "result.pdf",
+    margin: { top: "0", right: "0", bottom: "0", left: "0" },
+    printBackground: true,
+    format: "A4",
+  });
+  await browser.close();
+}
+
 async function clean() {
   await deleteAsync(path.join(__dirname, "dist/"));
 
   await mkdir(path.join(__dirname, "dist/css"), { recursive: true });
 }
 
-const task = series(clean, parallel(buildHtml, buildCss));
-watch(
-  [
-    path.normalize("templates/*"),
-    path.normalize("css/*"),
-    path.normalize("data/*"),
-  ],
-  task,
-);
-
-export default task;
+export default () =>
+  watch(
+    [
+      path.normalize("templates/*"),
+      path.normalize("css/*"),
+      path.normalize("data/*"),
+    ],
+    { ignoreInitial: false },
+    series(clean, parallel(buildHtml, buildCss)),
+  );
